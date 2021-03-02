108bca90ad1efac9c3c8c6d6e2e75999e3137d91
rodrigues@novusti.com <thiagonr86@gmail.com>
2021-03-02 13:01:38 -0300

    removing unecessary

--- src/App.vue
+++ src/App.vue
@@ -19,28 +19,3 @@ export default class App extends Vue {
   
 }
 </script>
-
-<style>
-#app {
-  /* font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50; */
-  /* margin-right: 10px;
-  margin-left: 10px; */
-}
-
-/* #nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-} */
-</style>
