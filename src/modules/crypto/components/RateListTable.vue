4444192e3e77865774a58cb4771ab9fcafc2e8c9
rodrigues@novusti.com <thiagonr86@gmail.com>
2021-03-02 17:11:47 -0300

    layout adjustments

--- src/modules/crypto/components/RateListTable.vue
+++ src/modules/crypto/components/RateListTable.vue
@@ -1,24 +1,24 @@
 <template>
   <table class="table-auto w-full">
     <thead>
-      <tr class="bg-purple-400 text-white">
-        <th class="text-left py-2 pl-4 pr-2">#</th>
-        <th class="text-left p-2">Coin</th>
-        <th class="text-left p-2">Price</th>
-        <th class="text-left p-2">Total Vol. 24h</th>
+      <tr class="bg-yellow-100">
+        <th class="text-left py-2 pl-4 pr-2 text-yellow-600">#</th>
+        <th class="text-left p-2 text-yellow-600">Coin</th>
+        <th class="text-left p-2 text-yellow-600">Price</th>
+        <th class="text-left p-2 text-yellow-600">Total Vol. 24h</th>
       </tr>
     </thead>
     <tbody>
       <tr
         v-for="(currency, index) in currencies"
         v-bind:key="'rlt' + index"
-        :class="index % 2 === 0 ? 'bg-purple-200' : 'bg-purple-300'"
+        :class="index % 2 === 1 ? 'bg-purple-100' : 'bg-purple-200'"
       >
         <td class="text-left py-1 pl-4 pr-2">
-          <p class="text-gray-500 font-bold">{{ index + 1 }}</p>
+          <p class="text-purple-500 font-bold">{{ index + 1 }}</p>
         </td>
         <td class="text-left py-1 px-2">{{ currency.shortName }}</td>
-        <td class="text-left py-1 px-2 text-green-800 ">
+        <td class="text-left py-1 px-2 text-purple-500 font-bold">
           {{ _formatPrice(currency.rates[0].price, currency.rates[0].shortName) }}
         </td>
         <td class="text-left py-1 px-2">
